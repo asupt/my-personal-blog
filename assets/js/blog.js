cee6bb3e4eda24a945ad7353b57b85f03d1a8351
asup <asuptv@gmail.com>
2024-04-01 17:58:09 -0400

    Completed blog post functionality

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -41,8 +41,62 @@ else {
 
 localStorage.setItem('newPost', 'false');
 
-console.log(localStorage.getItem('fullBlog'));
+var blogArray = JSON.parse(localStorage.getItem('fullBlog'));
+console.log(blogArray);
+
+////////////// Creating elements on the blog //////////////
+
+const blogBody = document.querySelector('#blogBody');
+
+var idNumber = 0;
+
+// iterate through objects in the stored array of blog posts
+
+for (i of blogArray) {
+console.log(i);
+// sequential ID generation
+
+idNumber += 1;
+idNew = "b"+idNumber
+
+localStorage.setItem('id', idNew);
+
+//
+
+var item = document.createElement("div");
+item.className = 'item'; 
+item.setAttribute('id', idNew);
+
+var itemTitle = document.createElement("h2");
+itemTitle.className = 'postData';
+itemTitle.setAttribute('id', 'title');
+itemTitle.textContent = i.title;
+
+var itemContent = document.createElement("p");
+itemContent.className = 'postData';
+itemContent.setAttribute('id', 'content');
+itemContent.textContent = i.content;
+
+var extraSpace = document.createElement("p");
+
+var itemUsername = document.createElement("h3");
+itemUsername.className = 'postData';
+itemUsername.setAttribute('id', 'username');
+itemUsername.textContent = i.username;
+
+blogBody.appendChild(item);
+
+// get location of newest parent post
+newPost = document.querySelector('#'+idNew);
+
+newPost.appendChild(itemTitle);
+newPost.appendChild(itemContent);
+newPost.appendChild(extraSpace);
+newPost.appendChild(itemUsername);
 
 }
+
+}
+
 
 // localStorage.clear();
